cc8911ff4fb328a340ee6f80bfd69e924a6c8bcf
wictor <wictor@hotmail.com.br>
2018-10-21 19:21:00 -0300

    Reformulação da estrutura do projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,22 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { AboutComponent } from './about/about.component';
+import { MainComponent } from './main/main.component';
+import { TopComponent } from './main/top/top.component';
+import { HeadComponent } from './main/head/head.component';
+import { FooterComponent } from './main/footer/footer.component';
+import { MenuComponent } from './main/top/menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    MainComponent,
+    TopComponent,
+    HeadComponent,
+    FooterComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
